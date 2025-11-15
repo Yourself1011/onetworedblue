3f627c957339aca1ad94ece12b6ec4c7c475e53a
Daniel Zhang <d.zhang200788@gmail.com>
2025-11-15 03:14:47 -0500

    nnue

--- src/nnue.py
+++ src/nnue.py
@@ -10,13 +10,14 @@ with image.imports():  # set up common imports
     import torch
     from torch import int32
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 # Here, we define the config as a dictionary so that we can re-use it here
 # and later, when we attach the profiler. We want to make sure the profiler is in the same environment!
 
 config = {"gpu": "a10g", "image": image}
 
 
-@app.function(**config)
 def feedforward(
     board,
     w1,
@@ -35,26 +36,19 @@ def feedforward(
             0,
             1,
         )
-        ** 2
         @ w2
         + b2
     )
 
 
 @app.function(**config)
-def autograd(loss, optim):
-    loss.backwards()
-    optim.step()
-
-
-@app.local_entrypoint()
-def main():
+def train():
     HIDDEN_WIDTH = 1024
-    w1 = torch.empty((12 * 64, HIDDEN_WIDTH), requires_grad=True)
-    b1 = torch.zeros(HIDDEN_WIDTH, requires_grad=True)
-    w2 = torch.empty((HIDDEN_WIDTH * 2, 1), requires_grad=True)
-    b2 = torch.zeros(1, requires_grad=True)
-    board = torch.randint(0, 2, (12, 8, 8), dtype=torch.float32)
+    w1 = torch.empty((12 * 64, HIDDEN_WIDTH), requires_grad=True, device=device)
+    b1 = torch.zeros(HIDDEN_WIDTH, requires_grad=True, device=device)
+    w2 = torch.empty((HIDDEN_WIDTH * 2, 1), requires_grad=True, device=device)
+    b2 = torch.zeros(1, requires_grad=True, device=device)
+    board = torch.randint(0, 2, (12, 8, 8), dtype=torch.float32, device=device)
 
     with torch.no_grad():
         w1.uniform_(-0.001, 0.001)
@@ -62,6 +56,20 @@ def main():
 
     optim = torch.optim.AdamW([w1, b1, w2, b2])
 
-    print(feedforward.remote(board, w1, b1, w2, b2))
+    for i in range(1000):
+        optim.zero_grad()
+        if not i % 1000:
+            print(i)
+        output = feedforward(board, w1, b1, w2, b2)
+        loss = (1 - output) ** 2
+        loss.backward()
+        optim.step()
 
+    output = feedforward(board, w1, b1, w2, b2)
+    print(output)
+
+
+@app.local_entrypoint()
+def main():
+    train.remote()
     print("wenis")
