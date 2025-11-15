8d7435403cfa5232cf6ace86f0b1d78e42d181c1
Daniel Zhang <d.zhang200788@gmail.com>
2025-11-15 12:15:29 -0500

    saving

--- src/nnue.py
+++ src/nnue.py
@@ -1,4 +1,8 @@
 import modal
+from pathlib import Path
+
+weights = modal.Volume.from_name("weights", create_if_missing=True)
+WEIGHTS_DIR = Path("/weights")
 
 app = modal.App("onetworedblue")
 
@@ -15,7 +19,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Here, we define the config as a dictionary so that we can re-use it here
 # and later, when we attach the profiler. We want to make sure the profiler is in the same environment!
 
-config = {"gpu": "a10g", "image": image}
+config = {"gpu": "a10g", "image": image, "volumes": {WEIGHTS_DIR: weights}}
 
 
 def feedforward(
@@ -36,37 +40,66 @@ def feedforward(
             0,
             1,
         )
+        ** 2
         @ w2
         + b2
     )
 
 
+def save(w1, b1, w2, b2, optim):
+    obj = {
+        "weights": {"w1": w1, "b1": b1, "w2": w2, "b2": b2},
+        "optim": optim.state_dict(),
+    }
+    torch.save(obj, WEIGHTS_DIR / "save.pth")
+
+
+def load():
+    try:
+        obj = torch.load(WEIGHTS_DIR / "save.pth")
+        w1 = obj["weights"]["w1"]
+        b1 = obj["weights"]["b1"]
+        w2 = obj["weights"]["w2"]
+        b2 = obj["weights"]["b2"]
+        optim = obj["weights"]["optim"]
+    except Exception as e:
+        print(e)
+        HIDDEN_WIDTH = 1024
+        w1 = torch.empty((12 * 64, HIDDEN_WIDTH), requires_grad=True, device=device)
+        b1 = torch.zeros(HIDDEN_WIDTH, requires_grad=True, device=device)
+        w2 = torch.empty((HIDDEN_WIDTH * 2, 1), requires_grad=True, device=device)
+        b2 = torch.zeros(1, requires_grad=True, device=device)
+        optim = torch.optim.AdamW([w1, b1, w2, b2], lr=3e-3)
+    return w1, b1, w2, b2, optim
+
+
 @app.function(**config)
 def train():
     dataset = loadDb()
-    HIDDEN_WIDTH = 1024
-    w1 = torch.empty((12 * 64, HIDDEN_WIDTH), requires_grad=True, device=device)
-    b1 = torch.zeros(HIDDEN_WIDTH, requires_grad=True, device=device)
-    w2 = torch.empty((HIDDEN_WIDTH * 2, 1), requires_grad=True, device=device)
-    b2 = torch.zeros(1, requires_grad=True, device=device)
+    WEIGHTS_DIR.mkdir(parents=True, exist_ok=True)
+    w1, b1, w2, b2, optim = load()
     board = torch.randint(0, 2, (12, 8, 8), dtype=torch.float32, device=device)
 
     with torch.no_grad():
         w1.uniform_(-0.001, 0.001)
         w2.uniform_(-0.001, 0.001)
 
-    optim = torch.optim.AdamW([w1, b1, w2, b2])
-
-    for i in range(10000):
+    totalLoss = 0
+    n = 1000
+    for i in range(50000):
         optim.zero_grad()
-        if not i % 1000:
-            print(i)
+        if not i % n:
+            save(w1, b1, w2, b2, optim)
+            print(i, "loss:", totalLoss / n)
+            totalLoss = 0
 
         evaluation = getData(dataset)
         board = fen_to_tensor(evaluation["fen"], device)
 
         output = feedforward(board, w1, b1, w2, b2)
-        loss = (evaluation["cp"] - output) ** 2
+        target = evaluation["cp"] / 100
+        loss = (target - output) ** 2
+        totalLoss += loss
         loss.backward()
         optim.step()
 
