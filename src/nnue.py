3bf1dbbcad3d96397f8cc0440824adb607e0a09e
Daniel Zhang <d.zhang200788@gmail.com>
2025-11-14 22:37:38 -0500

    modal example

---
+++ src/nnue.py
@@ -0,0 +1,15 @@
+import modal
+import torch
+
+app = modal.App("onetworedblue")
+
+
+@app.function()
+def square(x):
+    print("This code is running on a remote worker!")
+    return x**2
+
+
+@app.local_entrypoint()
+def main():
+    print("the square is", square.remote(42))
